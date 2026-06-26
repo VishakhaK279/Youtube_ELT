67a65c23eb08109895117e40aa220a32087f9158
Vishakha Kokate <vishakhakokate2@gmail.com>
2026-06-26 08:27:04 +0530

    Extracted video data

--- video_stats.py
+++ video_stats.py
@@ -70,7 +70,54 @@ def get_video_ids(playlist_id):
     except requests.exceptions.RequestException as e:
         raise e  
 
+
+def extract_video_data(video_id):
+    extracted_data = []
+
+    def batch_list(video_lst, batch_size):
+        for start in range(0, len(video_lst), batch_size):
+            yield video_lst[start:start + batch_size]
+
+    try:
+        for batch in batch_list(video_id, 50):
+            video_ids = ','.join(batch)
+
+            url = (
+                f'https://youtube.googleapis.com/youtube/v3/videos?'
+                f'part=snippet,statistics&id={video_ids}&key={API_KEY}'
+            )
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('items',[]):
+                video_id = item['id']
+                snippet = item['snippet']
+                content_details = item.get('contentDetails')
+                duration = content_details.get('duration') if content_details else None
+                statistics = item['statistics']
+
+            video_data = {
+                "video_id": video_id,
+                "title":snippet['title'],
+                "publishedAt": snippet['publishedAt'],
+                "duration": duration,
+                "viewCount": statistics.get('viewCount', None),
+                "likeCount": statistics.get('likeCount', None),
+                "commentCount": statistics.get('commentCount', None)
+            }
+
+            extracted_data.append(video_data)
+
+        return extracted_data
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
 if __name__=="__main__":
-      playlist_id = get_Playlist_id()
-      videoIds = get_video_ids(playlist_id)
-      print(videoIds)
+    playlist_id = get_Playlist_id()
+    video_ids = get_video_ids(playlist_id)
+    video_data = extract_video_data(video_ids)
+    print(json.dumps(video_data[:3], indent=2))
+
